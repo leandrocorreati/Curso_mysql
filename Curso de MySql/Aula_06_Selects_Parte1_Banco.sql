c85bb130dc4f5522d8676d27d5e56efd43dc66c2
leandro_correa <leandrocorreati@gmail.com>
2017-04-02 21:09:27 -0300

    1.0

---
+++ Curso de MySql/Aula_06_Selects_Parte1_Banco.sql
@@ -0,0 +1,37 @@
+/* COMANDOS DQL */
+
+select * from gafanhotos;
+
+select * from cursos;
+
+select * from cursos order by nome;
+
+select * from cursos order by nome asc;
+
+select * from cursos order by nome desc;
+
+select nome,carga,ano from cursos order by nome;
+
+select ano,nome,carga from cursos order by ano,nome;
+
+select * from cursos where ano = '2016' order by nome;
+
+select nome,carga from cursos where ano = '2016' order by nome;
+
+select nome, descricao, carga from cursos where ano = '2016' order by nome;
+
+select nome, descricao from cursos where ano <= '2015' order by nome;
+
+select nome, descricao,ano from cursos where ano != '2015' order by nome, ano;
+
+select nome, ano from cursos where ano between 2014 and 2016;
+
+select nome, ano from cursos where ano between 2014 and 2016 order by ano desc, nome;
+
+select nome, descricao,ano from cursos where ano in(2014,2016) order by ano;
+
+select nome, descricao,ano from cursos where ano in(2014,2016,2020) order by ano;
+
+select nome,carga from cursos where carga > 35 and totaulas < 30;
+
+select nome,carga from cursos where carga > 35 or totaulas < 30;
